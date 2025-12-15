4c80d76ff3ad259bdf4eac54ccc86ab957e1f98d
6804062612013 <s6804062612013@email.kmutnb.ac.th>
2025-12-15 10:00:42 +0700

    solution 2

---
+++ Lab3 6.2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+	int score ;
+	scanf("%d",&score) ;
+	if  (score < 68)  {
+	  if (score < 55) {
+		printf("F") ; 
+	} else {
+		printf("D") ; 
+	} }
+    else if (score >= 85) {
+		printf("A");
+  	} else if (score >= 75) {
+		printf("B");
+  	} else {
+		printf("C") ;
+		}
+	}
